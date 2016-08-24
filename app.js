c8853b4bfd5c0f4f5e31aa76ca70589936dda08e
Julian Rudolph <julian.rudolphsc@gmail.com>
2016-08-24 03:32:01 +0000

    Basic shell, added mongoose

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ var mongoose = require('mongoose');
 var app = express();
 
 //mongoose setup
+mongoose.connect('mongodb://localhost/travelphotoapp');
+
+var photoSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Photo = new mongoose.model('Photo', photoSchema);
 
 //app setup
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended: true}));
 
 //routes
 
