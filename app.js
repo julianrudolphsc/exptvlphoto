8429e3214ff1f2fde9ea30fffd444e983642e8a6
Julian Rudolph <julian.rudolphsc@gmail.com>
2016-08-24 03:25:31 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var app = express();
+
+//mongoose setup
+
+//app setup
+
+//routes
+
+//listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server Started!");
+});
